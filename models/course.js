f82e78f4e8b725b78a593bad6a51caed161d9b76
Павел <pavel.tagiltsef@gmail.com>
2021-10-03 13:28:34 +0300

    Редактирование курса

--- models/course.js
+++ models/course.js
@@ -19,6 +19,27 @@ class Course {
         }
     }
 
+    static async update(course) {
+        const courses = await Course.getAll()
+
+        const index = courses.findIndex((item) => item.id === course.id)
+        courses[index] = course
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                JSON.stringify(courses),
+                (err) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve()
+                    }
+                }
+            )
+        })
+    }
+
     async save() {
         const courses = await Course.getAll()
         courses.push(this.toJSON())
