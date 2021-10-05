58ef6e7fdc99b2f643d68bbe9b431e71748d4115
Павел <pavel.tagiltsef@gmail.com>
2021-10-05 22:18:57 +0300

    Отображение корзины

--- routes/cart.js
+++ routes/cart.js
@@ -2,6 +2,12 @@ const {Router} = require('express')
 const Course = require('../models/course')
 const router = Router()
 
+function mapCartItems(cart) {
+    return cart.items.map((item) => ({
+        ...item.courseId._doc, count: item.count
+    }))
+}
+
 router.post('/add', async (req, res) => {
     const course = await Course.findById(req.body.id)
     await req.user.addToCart(course)
@@ -14,17 +20,17 @@ router.delete('/remove/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-    // const cart = await Cart.fetch()
-    // const {price, courses} = cart
+    const user = await req.user
+        .populate('cart.items.courseId');
+    
+    const courses = mapCartItems(user.cart)
 
-    // res.render('cart', {
-    //     title: 'Корзина',
-    //     isCart: true,
-    //     courses,
-    //     price
-    // })
-
-    res.json({test: true})
+    res.render('cart', {
+        title: 'Корзина',
+        isCart: true,
+        courses,
+        price: 0
+    })
 })
 
 module.exports = router
