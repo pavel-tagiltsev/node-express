c5a30346295932a8bbe72b61bd83f54efb8b17cf
Павел <pavel.tagiltsef@gmail.com>
2021-10-21 20:33:07 +0300

    Отображение корзины

--- routes/cart.js
+++ routes/cart.js
@@ -8,6 +8,12 @@ function mapCartItems(cart) {
     }))
 }
 
+function computePrice(courses) {
+    return courses.reduce((total, course) => {
+        return total += course.price * course.count
+    }, 0)
+}
+
 router.post('/add', async (req, res) => {
     const course = await Course.findById(req.body.id)
     await req.user.addToCart(course)
@@ -29,7 +35,7 @@ router.get('/', async (req, res) => {
         title: 'Корзина',
         isCart: true,
         courses,
-        price: 0
+        price: computePrice(courses)
     })
 })
 
