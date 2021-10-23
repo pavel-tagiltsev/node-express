07e7a80ec0bb1e62ff1160df1edf2c1ed465b566
Павел <pavel.tagiltsef@gmail.com>
2021-10-23 17:46:50 +0300

    Регистрация пользователя

--- routes/auth.js
+++ routes/auth.js
@@ -27,4 +27,25 @@ router.post('/login', async (req, res) => {
     })
 })
 
+router.post('/register', async (req, res) => {
+    try {
+        const {email, password, repeat, name} = req.body
+        const candidate = await User.findOne({ email })
+
+        if (candidate) {
+            res.redirect('/auth/login#register')
+        } else {
+            const user = new User({
+                email, name, password, cart: {items: []}
+            })
+
+            await user.save()
+            res.redirect('/auth/login#login')
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 module.exports = router
